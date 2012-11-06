8369b37f4b0e28644f05d4e40d88d401a2bea790
chisun-joung <csjoung@gmail.com>
2012-11-05 23:28:32 -0800

    package

---
+++ 20121106/java/lg/App.java
@@ -0,0 +1,11 @@
+package com;
+import com.*;
+
+public class App
+{
+	public static void main(String [] arg)
+	{
+		Paint p = new Paint();
+		p.draw();
+	}
+}
---
+++ 20121106/java/lg/Paint.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Paint
+{
+	public void draw()
+	{
+		System.out.println("draw");
+	}
+}
