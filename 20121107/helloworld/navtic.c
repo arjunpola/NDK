25f522aaf4ddc707f9ed19d1ef3f73033d83e768
chisun-joung <csjoung@gmail.com>
2012-11-06 15:32:25 -0800

    without package for teaching purpose

--- 20121107/helloworld/navtic.c
+++ 20121107/helloworld/navtic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <jni.h>
 
-void Java_test_Main_printHelloWorld(JNIEnv *pEnv,jclass clazz)
+void Java_Main_printHelloWorld(JNIEnv *pEnv,jclass clazz)
 {
 	printf("[C] Hello world \n");
 }
