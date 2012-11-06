ef5861beea3a947e22d2581efbd2802d97c02b58
chisun-joung <csjoung@gmail.com>
2012-11-05 22:32:57 -0800

    array

---
+++ 20121106/java/Array.java
@@ -0,0 +1,14 @@
+
+public class Array
+{
+	public static void main(String arg[]) 
+	{
+		// it is ok only for initialize
+		int[] arr = { 1,2,3,4,5,6,7,8,9};
+		
+		for( int  i : arr )
+		{
+			System.out.println("[" + i + "]");
+		}
+	}
+}
