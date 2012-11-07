4fa1c20c8eba61bf693b1f1eca8ad97fbff89523
chisun-joung <csjoung@gmail.com>
2012-11-06 23:20:13 -0800

    get static field in JNI

--- 20121107/native2/Main.c
+++ 20121107/native2/Main.c
@@ -1,14 +1,22 @@
 #include "Main.h"
+#include <stdio.h>
 
 JNIEXPORT jfloat JNICALL Java_Main_getCircleArea
   (JNIEnv *pEnv, jclass clazz, jint r)
 {
 	jclass CircleClass;
 	jmethodID getCircleArea;
+	jfieldID PI;
 	jfloat result;
+	jfloat f;
 
 	CircleClass = (*pEnv)->FindClass(pEnv, "Circle");
 	getCircleArea = (*pEnv)->GetStaticMethodID(pEnv, CircleClass, "getCircleArea","(I)F");
+
+	// call static field 
+	PI = (*pEnv)->GetStaticFieldID(pEnv, CircleClass, "PI", "F");
+	f = (*pEnv)->GetStaticFloatField(pEnv, CircleClass, PI);
+    printf("[C] PI of Circle Class = %f \n",f);
 
 	result = (*pEnv)->CallStaticFloatMethod(pEnv, CircleClass, getCircleArea, r);
 	return result;
